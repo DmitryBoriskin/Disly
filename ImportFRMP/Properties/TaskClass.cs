9f9b017bfc71eb47450e1f36e0d5c0c965f4340c
DmitryBoriskin <dmitry@boriskiny.ru>
2017-10-03 08:43:58 +0300

    service import

---
+++ ImportFRMP/Properties/TaskClass.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace ImportFRMP
+{
+    internal sealed class TaskClass
+    {
+        private Thread taskThread;
+        private Boolean threadEnabled;
+        private TimeSpan timeToStart;
+        private ThreadStart workMetod;
+
+        /// <summary>
+        /// Активность потока
+        /// </summary>
+        public Boolean ThreadEnabled
+        {
+            get { return this.threadEnabled; }
+            set { this.threadEnabled = value; }
+        }
+
+        /// <summary>
+        /// Конструктор
+        /// </summary>
+        /// <param name="method">Метод для вызова</param>
+        /// <param name="timeToStart">Время запуска</param>
+        public TaskClass(ThreadStart method, TimeSpan timeToStart)
+        {
+            this.taskThread = new Thread(WorkMeth);
+
+            this.workMetod = method;
+            this.timeToStart = timeToStart;
+        }
+
+        /// <summary>
+        /// Запуск потока
+        /// </summary>
+        public void Start()
+        {
+            this.threadEnabled = true;
+            this.taskThread.Start();
+        }
+
+        /// <summary>
+        /// Остановка потока
+        /// </summary>
+        public void Stop()
+        {
+            this.threadEnabled = false;
+
+            try
+            {
+                this.taskThread.Join(5000);
+            }
+            catch
+            {
+                try
+                {
+                    this.taskThread.Abort();
+                }
+                catch { }
+            }
+        }
+
+        private void WorkMeth()
+        {
+            while (this.threadEnabled)
+            {
+                if (this.workMetod != null)
+                    this.workMetod();
+
+                var nowTime = DateTime.Now.TimeOfDay;
+                var seconds = 0.0;
+                if (nowTime <= this.timeToStart)
+                    seconds = (this.timeToStart - nowTime).TotalSeconds;
+                else
+                    seconds = 24 * 60 * 60 - (nowTime - this.timeToStart).TotalSeconds;
+
+                Thread.Sleep((int)seconds * 1000);
+            }
+        }
+    }
+}
---
+++ ImportFRMP/create_classes_from_xsd.bat
@@ -0,0 +1,1 @@
+"C:\Program Files\Microsoft SDKs\Windows\v6.0A\Bin\xsd.exe" "XSD\schema0.xsd" /c /l:CS /n:ImportFedMedEmployees.XSD /o:XSD
