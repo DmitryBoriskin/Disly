b771f56f9071836ea3dcf36c14214509386b32b9
travis88 <hellsballs99@gmail.com>
2017-10-03 08:49:30 +0300

    added comment to repository for sitemap

---
+++ ImportFRMP/Properties/ArrayOfEmployee.cs
@@ -0,0 +1,33 @@
+﻿using ImportFedMedEmployees.XSD;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace ImportFRMP.XSD
+{
+    [Serializable()]
+    [System.Diagnostics.DebuggerStepThrough()]
+    [System.ComponentModel.DesignerCategory("code")]
+    [XmlRoot(Namespace = "Employee", IsNullable = true)]
+    public partial class ArrayOfEmployee
+    {
+        private Employee[] employees;
+
+        /// <remarks/>
+        [XmlElement(ElementName = "Employee")]
+        public Employee[] Employees
+        {
+            get
+            {
+                return this.employees;
+            }
+            set
+            {
+                this.employees = value;
+            }
+        }
+
+    }
+}
