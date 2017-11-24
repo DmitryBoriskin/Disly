ab14204642cbd705e5c18553486f4ee9b995ec8b
ailo21 <alex92ailo@mail.ru>
2017-11-24 15:48:47 +0300

    less introduction

---
+++ Disly/gulpfile.js
@@ -0,0 +1,16 @@
+/// <binding BeforeBuild='first' />
+"use strict";
+
+var gulp = require("gulp"),
+    rimraf = require("rimraf"),
+    less = require("gulp-less"); // добавляем модуль less
+
+var paths = {
+    webroot: "./Content/"
+};
+//  регистрируем задачу по преобразованию styles.less в файл css
+gulp.task("first", function () {
+    return gulp.src('content/css/theme/first.less')
+               .pipe(less())
+               .pipe(gulp.dest(paths.webroot + 'css/theme'))
+});
