46b3bb56edab6230a6400859e1aeaafc45ea344e
hrum-hrum <svetlana.kaznacheeva@gmail.com>
2018-02-07 12:13:58 +0300

    f2

--- Disly/Areas/Admin/Controllers/SitesController.cs
+++ Disly/Areas/Admin/Controllers/SitesController.cs
@@ -145,15 +145,11 @@ namespace Disly.Areas.Admin.Controllers
         /// <returns></returns>
         [HttpPost]
         [MultiButton(MatchFormKey = "action", MatchFormValue = "search-btn")]
-        public ActionResult Search(string searchtext, bool enabled, string size, DateTime? date, DateTime? dateend)
+        public ActionResult Search(string searchtext, bool enabled, string size)
         {
             string query = HttpUtility.UrlDecode(Request.Url.Query);
             query = AddFiltrParam(query, "searchtext", searchtext);
             query = AddFiltrParam(query, "disabled", (!enabled).ToString().ToLower());
-            if (date.HasValue)
-                query = AddFiltrParam(query, "date", date.Value.ToString("dd.MM.yyyy").ToLower());
-            if (dateend.HasValue)
-                query = AddFiltrParam(query, "dateend", dateend.Value.ToString("dd.MM.yyyy").ToLower());
             query = AddFiltrParam(query, "page", String.Empty);
             query = AddFiltrParam(query, "size", size);
 
